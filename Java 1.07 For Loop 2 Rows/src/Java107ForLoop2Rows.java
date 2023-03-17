a4cace9e5dbff4aba45ef7e09faab58fed04ead1
Kazureee <109503455+Kazureee@users.noreply.github.com>
2023-03-17 18:40:21 +0800

    Java 1.07 For Loop 2 Rows

---
+++ Java 1.07 For Loop 2 Rows/src/Java107ForLoop2Rows.java
@@ -0,0 +1,14 @@
+public class Java107ForLoop2Rows {
+
+    public static void main(String[] args) {
+        char letter = 'a';
+            for (int i = 1; i <= 2; i++) {
+                for (int j = 1; j <= 4; j++) {
+                    System.out.print(letter + " ");
+            }
+        System.out.println();
+}
+
+    }
+    
+}
