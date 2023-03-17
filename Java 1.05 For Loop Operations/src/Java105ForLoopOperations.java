603af761285b470b16dac54675de70d9f9f1f8f3
Kazureee <109503455+Kazureee@users.noreply.github.com>
2023-03-17 18:33:01 +0800

    Java 1.05 For Loop Operations

---
+++ Java 1.05 For Loop Operations/src/Java105ForLoopOperations.java
@@ -0,0 +1,8 @@
+public class Java105ForLoopOperations {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++)    // repeat 5 times
+            System.out.println("Sum is: " + i*(i+1)/2);
+    }
+    
+}
