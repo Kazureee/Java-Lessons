80cb262ef03402b2500becf80d6de111ceedeadd
Kazureee <109503455+Kazureee@users.noreply.github.com>
2023-03-17 18:35:05 +0800

    Java 1.06 While Loop

---
+++ Java 1.06 While Loop/src/Java106WhileLoop.java
@@ -0,0 +1,11 @@
+public class Java106WhileLoop {
+
+    public static void main(String[] args) {
+        int x = 1;
+        while (x <= 6)  {
+        System.out.println("The sum is : " + x);
+    x++; 
+        }
+    }
+    
+}
