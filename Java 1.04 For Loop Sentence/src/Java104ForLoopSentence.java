195872866decbd0e8cb0d115a1b5d1946a958a68
Kazureee <109503455+Kazureee@users.noreply.github.com>
2023-03-17 18:31:10 +0800

    Java 1.04 For Loop Sentence

---
+++ Java 1.04 For Loop Sentence/src/Java104ForLoopSentence.java
@@ -0,0 +1,10 @@
+public class Java104ForLoopSentence {
+
+    public static void main(String[] args) {
+        for (int i= 1; i<= 5; i++) {   // repeat 5 times
+            System.out.println("I am so smart");}
+            System.out.println("S-M-R-T");
+            System.out.println("I mean S-M-A-R-T");
+    }
+    
+}
