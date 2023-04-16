f472113d2109e0ba96e3f9d1112c70fe7775535b
Kazureee <109503455+Kazureee@users.noreply.github.com>
2023-04-16 23:27:17 +0800

    Java 1.20 Encapsulation

---
+++ Java 1.20 Encapsulation/src/Encapsulation.java
@@ -0,0 +1,8 @@
+public class Encapsulation {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Juan Dela Cruz");
+        System.out.println("Name: " + s1.getName());    
+    }
+    
+}
---
+++ Java 1.20 Encapsulation/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    //variable/s are declared as private
+    private String name;
+    //public getter/s and setter/s are provided
+    public String getName(){
+        return name; //return the privite name
+    }
+    public void setName(String name){ // since this is void it will recieve the value
+        this.name = name;
+    }
+}
