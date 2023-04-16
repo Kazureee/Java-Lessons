4167ce2b2c38ba77a9d818b4794766350331c873
Kazureee <109503455+Kazureee@users.noreply.github.com>
2023-04-16 23:30:23 +0800

    Java 1.22 Inheritance

---
+++ Java 1.22 Inheritance/src/Inheritance.java
@@ -0,0 +1,11 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        Student student = new Student(); //parent class         
+        Student1 student1 = new Student1();       
+        Student2 student2 = new Student2();     
+        student.displayName();  
+        student1.displayName();
+        student2.displayName();
+    }
+    
+}
---
+++ Java 1.22 Inheritance/src/Student.java
@@ -0,0 +1,8 @@
+//Parent Class
+//Method overwriting = overwrite in multiple classes while method overloading overwrite in the same class
+public class Student {
+    public String course = "BSCS-ML";
+    public  void displayName(){
+        System.out.println("Student Name\tCourse");
+    }
+}
