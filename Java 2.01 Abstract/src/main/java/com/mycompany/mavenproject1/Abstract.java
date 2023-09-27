0b5b93d56bee802e6a17e86506fe05ee5af6e534
Kazureee <109503455+Kazureee@users.noreply.github.com>
2023-09-28 00:03:47 +0800

    Java 2.01 Abstract

---
+++ Java 2.01 Abstract/src/main/java/com/mycompany/mavenproject1/Abstract.java
@@ -0,0 +1,7 @@
+package com.mycompany.mavenproject1;
+abstract class Animal {
+    public abstract void animalSound();
+    public void sleep(){
+        System.out.println("Zzz");
+    }
+}
---
+++ Java 2.01 Abstract/src/main/java/com/mycompany/mavenproject1/Pig.java
@@ -0,0 +1,6 @@
+package com.mycompany.mavenproject1;
+class Pig extends Animal{
+    public void animalSound(){
+        System.out.println("The pigs say: wee wee");
+    }
+}
